3dc1170c1aa112d037983ac2864e3fd0179c1dbe
GCharles85 <gcharles345@gmail.com>
2024-12-10 20:20:57 -0500

    add list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import List from './List';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  const fruits = [{id:1, name:'apple', calories:90},
+                  {id:2, name:'banana', calories:99},
+                  {id:3, name:'grapes', calories:45},
+                  {id:4, name:'orange', calories:45},
+                  {id:5, name:'mango', calories:60}]
 
-  return (
+  const vegetables = [{id:6, name:'potatoes', calories:77},
+                      {id:7, name:'tomatoes', calories:118},
+                      {id:8, name:'carrots', calories:41},
+                      {id:9, name: 'spinach', calories:23},
+                      {id:10, name:'broccoli', calories:90}]
+
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <List items={fruits} category="fruits"/>
+    </>);
 }
 
 export default App
