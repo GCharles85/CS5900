ef9415ae7af7db75a33bedc4e2e9f0acfb477fee
GCharles85 <gcharles345@gmail.com>
2024-12-21 18:53:06 -0500

    colorpicker

--- src/App.jsx
+++ src/App.jsx
@@ -21,71 +21,63 @@
 // onChange - event handler used primarily with form elements
 // ex: <input>, <textarea>, <select>, <radio>
 // Triggers a function every time the value of the input changes
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
+
+// function App(){
+//   const [name, setName] = useState("")
+//   const [payment, setPayment] = useState("")
+//   const [shipping, setShipping] = useState("")
+//   const [number, setNumber] = useState(0)
+//   const handleNameChange = (e) => {
+//     setName(e.target.value)
+//   }
+//   const handlePaymentChange = (e) => {
+//     setPayment(e.target.value)
+//   }
+//   const handleShippingChange = (e) => {
+//     setShipping(e.target.value)
+//   }
+//   const handleNumberChange = (e) => {
+//     setNumber(e.target.value)
+//   }
+ 
+//   return(
+//     <>
+//       <div>
+//         <input value={name} onChange={handleNameChange}>
+//         </input>
+//         <p>{name}</p>
+//         <select value={payment} onChange={handlePaymentChange}>
+//           <option value="Visa">Visa</option>
+//           <option value="MasterCard">MasterCard</option>
+//         </select>
+//         <p>{payment}</p>
+//         <label>
+//           <input type="radio" value="Pick Up" checked={shipping === "Pick Up"} onChange={handleShippingChange}/>
+//           Pick Up
+//         </label>
+//         <label>
+//           <input type="radio" value="Delivery" checked={shipping === "Delivery"} onChange={handleShippingChange}/>
+//           Delivery
+//         </label>
+//         <p>{shipping}</p>
+//         <label>
+//           <input type="number" value={number} onChange={handleNumberChange}/>
+//           <br/>
+//           Pick a number
+//         </label>
+//         <p>{number}</p>
+//         <textarea placeholder="enter delivery instructions"/>
+//         </div>
+//     </>
+//   );
+// }
+import ColorPicker from './ColorPicker';
 
 function App(){
-
-  const [name, setName] = useState("Guest");
-  const [quantity, setQuantity] = useState();
-  const [comment, setComment] = useState("");
-  const [payment, setPayment] = useState("Visa");
-  const [shipping, setShipping] = useState("");
-
-  function handleShippingChange(event){
-    setShipping(event.target.value);
-  }
-  function handlePaymentChange(event){
-    setPayment(event.target.value);
-  }
-  function handleCommentChange(event){
-    setComment(event.target.value);
-  }
-  function handleQuantityChange(event){
-    setQuantity(event.target.value);
-  }
-  function handleNameChange(event){
-    setName(event.target.value);
-  }
-
-  return(
-    <>
-      <div>
-        <input value={name} onChange={handleNameChange}/> 
-        <p>Name: {name}</p>
-
-        <input value={quantity} onChange={handleQuantityChange} type="number" />
-        <p>Quantity: {quantity}</p>
-
-        <textarea value={comment} onChange={handleCommentChange} placeholder="Enter delivery Instructions"></textarea>
-        <p>Comment: {comment}</p>
-
-        <select value={payment} onChange={handlePaymentChange}>
-          <option value="">Select an option</option>
-          <option value="Mastercard">Mastercard</option>
-          <option value="Giftcard">Giftcard</option>
-        </select>
-
-        <p>Payment: {payment}</p>
-
-        <label>
-          <input type="radio" value="Pick Up" 
-                 checked={shipping === "Pick Up"} 
-                 onChange={handleShippingChange}>
-          </input>
-          Pick Up
-        </label>
-        <br></br>
-        <label>
-        <input type="radio" value="Delivery" 
-                 checked={shipping === "Delivery"} 
-                 onChange={handleShippingChange}>
-          </input>
-          Delivery
-        </label>
-        <p>Shipping: {shipping}</p>
-      </div>
-    </>
-  );
+  return(<>
+  <ColorPicker />
+        
+        </>);
 }
-
 export default App
